cc9e2fd00286095f847418ed61614b46643ec0b2
Rene van Aerle <info@rvgate.nl>
2012-05-06 20:15:27 +0200

    Removed unused import

--- src/husaccttest/control/WorkspaceControllerTest.java
+++ src/husaccttest/control/WorkspaceControllerTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 import husacct.control.domain.Workspace;
-import husacct.control.presentation.workspace.SaveWorkspaceFrame;
 import husacct.control.task.MainController;
 import husacct.control.task.WorkspaceController;
 
